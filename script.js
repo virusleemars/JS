98fc1056b6fdc5887bce7842ff9f3cc86f037dd5
viruslee <virusleeandroid@gmail.com>
2019-07-19 00:34:49 +0400

    lesson2

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-alert('Hello Mediasoft!')
+﻿/*
+	Простой калькулятор
+	Вводятся два числа и оператор +-/*
+*/
+
+const OPERATOR_TRUE = '+-/*';
+
+// Функция ввода числа
+function inputNumber(text){
+	let number;
+	do{ 
+		number = prompt('введите ' + text, '');
+	}while (isNaN(number) || (number == null) || (number == '') );
+	return number;
+}
+
+// Функция проверки корректности оператора
+function isOperator(text){
+	if ( (text.length == 1) && (OPERATOR_TRUE.indexOf(text) >= 0) ) 
+		return true;
+	return false;
+}
+
+// Функция ввода оператора
+function inputOperator(text){
+	let oper;
+	do{ 
+		oper = prompt('введите ' + text, '');
+		oper = oper.trim();
+	}while ( (!isOperator(oper)) || (oper == null) || (oper == '') );
+	return oper;
+}
+
+
+let number1 = inputNumber('1 число');
+let number2 = inputNumber('2 число');
+let operator = inputOperator('оператор [+-/*]');
+
+switch(operator){
+	case '+':
+		alert(+number1 + +number2);
+	break;
+	
+	case '-':
+		alert(+number1 - +number2);
+	break;
+	
+	case '/':
+		if (+number2 != 0)
+			alert(+number1 / +number2);
+		else
+			alert('Деление на 0 !!!');
+	break;
+	
+	case '*':
+		alert(+number1 * +number2);
+	break;	
+	
+	default:
+		alert('ERROR ?!');
+	break;
+}
+
